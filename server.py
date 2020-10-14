cf5302c763ceb0342d5587d86322172a852f8f10
Eric <looicen66@gmail.com>
2020-10-14 15:25:13 +0800

    showed name from url parameter in template

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-   return render_template('index.html') # by default looks for index.html inside a templates folder in the same directory as this script.
+
+
+@app.route("/<name>")
+def index_name(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
 
 @app.route("/another")
