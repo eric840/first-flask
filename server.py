3fb3eda721ab9e5cc66853203e11c59ff96317f0
Eric <looicen66@gmail.com>
2020-10-14 14:51:31 +0800

    made show route show username

--- server.py
+++ server.py
@@ -10,5 +10,9 @@ def index():
 def show():
     return '<h1>Yo</h1>'
 
+@app.route('/user/<username>')
+def show_user(username):
+    return f"Hi {username[0:3]}"
+
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run()
